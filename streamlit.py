5653fd8e2f808587d6f867c5dcbb2cb582e3dd4c
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2024-08-26 00:59:29 +0530

    did some tinkering

--- streamlit.py
+++ streamlit.py
@@ -9,7 +9,7 @@ import pandas as pd
 st.write("Here's our first attempt at using data to create a table using pd : ")
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
+  'second column': [10, 20, 30, 45]
 })
 
 df
